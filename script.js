8f03704d3b967183e833122b457b440e90ecf84d
Raleydi Wardhana <166751493+wraleydi@users.noreply.github.com>
2024-04-23 18:10:02 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+const tombolMenu = document.querySelector('.toggle-menu')
+        const navBaru = document.querySelector('.navbar-new')
+
+        tombolMenu.onclick = function () {
+            navBaru.classList.toggle('is-active')
+        }
+
+        window.onscroll = function() {
+    navBaru.classList.remove('is-active');
+};
+
+        function showContent(contentId) {
+    document.getElementById("article2").classList.remove("hidden");
+    var sections = document.querySelectorAll("#article2 > section");
+    for (var i = 0; i < sections.length; i++) {
+        if (sections[i].id === contentId) {
+        sections[i].style.display = "block";
+        } else {
+        sections[i].style.display = "none";
+        }
+    }
+}
